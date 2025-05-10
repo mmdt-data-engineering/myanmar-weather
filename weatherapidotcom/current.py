9cd640ef2f007d793f8786d7da7f12de11b4b19f
Myo Thet <mr.myothet@gmail.com>
2025-05-10 13:00:10 +0700

    get data from weatherapidotcom

--- weatherapidotcom/current.py
+++ weatherapidotcom/current.py
@@ -33,7 +33,7 @@ def get_current_weather(df: pd.DataFrame) -> None:
 
         # Construct the API request URL
         url = f"{BASE_URL}?key={API_KEY}&q={latitude},{longitude}"
-        print(url)
+        # print(url)
 
         # Wait for 5 seconds to avoid hitting the API rate limit
         time.sleep(10)
@@ -54,17 +54,18 @@ def get_current_weather(df: pd.DataFrame) -> None:
 if __name__ == "__main__":
 
     # Get townships from MM-GEO data
-    township_df = get_town_data()
+    # township_df = get_town_data()
     # print(township_df.shape)
     # print(township_df.head(5))
-    township_df.to_csv("MM_GEO_townships.csv", index=False)
+    # township_df.to_csv("MM_GEO_townships.csv", index=False)
 
     # Get townships from MIMU data
-    # township_df = get_townships()
+    township_df = get_townships()
     # township_df.to_csv("MIMU_townships.csv", index=False)
 
     # township_df = township_df.head(3)
-    # weather_df = get_current_weather(township_df)
-    # print(weather_df)
+    weather_df = get_current_weather(township_df)
+    print(weather_df)
 
     # load_to_postgres(township_df, "townships")
+    weather_df.to_csv("weatherapidotcom_data.csv", index=False, header=True)
