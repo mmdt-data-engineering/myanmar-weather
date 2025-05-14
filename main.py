23ab45f81c5f37e772a29f7a6a093f1b4eecabf8
Myo Thet <mr.myothet@gmail.com>
2025-05-15 00:27:44 +0700

    open meteo daily sample response

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ if __name__ == "__main__":
 
     township_df = township_df.head(1)
 
-    weather_api = WeatherAPI()
-    weatherapi_current_df = weather_api.get_current(township_df)
-    print(weatherapi_current_df)
+    # weather_api = WeatherAPI()
+    # weatherapi_current_df = weather_api.get_current(township_df)
+    # print(weatherapi_current_df)
 
-    # load_to_postgres(township_df, "townships")
-    weatherapi_current_df.to_csv(
-        "./output/weatherapi_current.csv", index=False, header=True
-    )
+    # # load_to_postgres(township_df, "townships")
+    # weatherapi_current_df.to_csv(
+    #     "./output/weatherapi_current.csv", index=False, header=True
+    # )
 
-    weatherapi_daily_df = weather_api.get_daily(township_df, no_of_days=7)
-    print(weatherapi_daily_df)
-    weatherapi_daily_df.to_csv(
-        "./output/weatherapi_daily.csv", index=False, header=True
-    )
+    # weatherapi_daily_df = weather_api.get_daily(township_df, no_of_days=7)
+    # print(weatherapi_daily_df)
+    # weatherapi_daily_df.to_csv(
+    #     "./output/weatherapi_daily.csv", index=False, header=True
+    # )
 
     openmeteo_api = OpenMeteoAPI()
 
-    openmeteo_current_df = openmeteo_api.get_current(township_df)
-    openmeteo_current_df.to_csv(
-        "./output/open_meteo_current.csv", index=False, header=True
-    )
+    # openmeteo_current_df = openmeteo_api.get_current(township_df)
+    # openmeteo_current_df.to_csv(
+    #     "./output/open_meteo_current.csv", index=False, header=True
+    # )
 
     openmeteo_daily_df = openmeteo_api.get_daily(township_df)
     print(openmeteo_daily_df)
-    openmeteo_daily_df.to_csv("./output/open_meteo_daily.csv", index=False, header=True)
+    # openmeteo_daily_df.to_csv("./output/open_meteo_daily.csv", index=False, header=True)
