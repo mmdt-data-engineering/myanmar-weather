f84fe58bd9066efea1af324883b1b2b58a6b361b
Myo Thet <mr.myothet@gmail.com>
2025-05-15 16:23:25 +0700

    read all csv files in the output folder

--- db_utils.py
+++ db_utils.py
@@ -8,13 +8,48 @@ class PostgresDB:
     def __init__(self):
         pass
 
-    def load_to_postgres(df: pd.DataFrame, table_name: str) -> None:
+    def load_to_postgres(self, df: pd.DataFrame, table_name: str) -> None:
         """
-        Save the DataFrame to a PostgreSQL database.
+        Load the DataFrame to a PostgreSQL database.
+
+        Parameters:
+        df (DataFrame): DataFrame to be loaded
+        table_name (str): Table Name
+
+        Returns:
+        None
+
         """
         # Load environment variables
         load_dotenv()
         DB_URL = os.getenv("POSTGRES_URL")
+        print(f"POSTGRES_URL: {DB_URL}")
+
+        # Create a connection to the PostgreSQL database
+        engine = create_engine(DB_URL)
+        conn = engine.connect()
+
+        # Save the DataFrame to the database
+        df.to_sql(table_name, conn, if_exists="replace", index=False)
+
+        # Close the connection
+        conn.close()
+
+    def load_to_neon_postgres(self, df: pd.DataFrame, table_name: str) -> None:
+        """
+        Save the DataFrame to a Neon Server PostgreSQL
+
+        Parameters:
+        df (DataFrame): DataFrame to be loaded
+        table_name (str): Table Name
+
+        Returns:
+        None
+
+        """
+        # Load environment variables
+        load_dotenv()
+        DB_URL = os.getenv("NEON_POSTGRES_URL")
         print(f"POSTGRES_URL: {DB_URL}")
 
         # Create a connection to the PostgreSQL database
---
+++ load_to_db.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from db_utils import PostgresDB
+
+import os
+import pandas as pd
+
+# Set the directory containing the CSV files
+folder_path = "./output"
+# Check if the folder exists
+if not os.path.exists(folder_path):
+    raise FileNotFoundError(
+        f"Folder not found: {folder_path} (working dir: {os.getcwd()})"
+    )
+
+# List all CSV files in the folder
+csv_files = [f for f in os.listdir(folder_path) if f.endswith(".csv")]
+
+for file in csv_files:
+
+    df = pd.read_csv(os.path.join(folder_path, file))
+    df.Name = os.path.splitext(file)[0]
+
+    print(f"DataFrame Name: {df.Name}")
+    print(f"DataFrame Shape: {df.shape}")
+
+    # db = PostgresDB()
+
+    # Load the DataFrame to PostgreSQL
+    # db.load_to_postgres(df, table_name=df.Name)
+
+    # Load the DataFrame to Neon PostgreSQL
+    # db.load_to_neon_postgres(df, table_name=df.Name)
