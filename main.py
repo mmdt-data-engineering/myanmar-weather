4d411a6330603f1dec2b5bc4178b1b1da8993410
Myo Thet <mr.myothet@gmail.com>
2025-05-16 17:40:25 +0700

    refactor upload_to_s3 and load_to_db

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from data_utils import MIMU_Data
 from weather_api import WeatherAPI
 from openmeteo_api import OpenMeteoAPI
 from ambientweather_api import AmbientWeatherAPI
+from datetime import date
 
 
-if __name__ == "__main__":
-
+def fetch_weather_data():
     # Get townships from MIMU data
     mimu_data = MIMU_Data()
     township_df = mimu_data.get_townships()
@@ -14,29 +14,38 @@ if __name__ == "__main__":
 
     weather_api = WeatherAPI()
 
+    today = date.today()
+    str_today = today.strftime("%Y-%m-%d")  # Output like '2025-05-16'
+
     # weatherapi - current
     weatherapi_current_df = weather_api.get_current(township_df)
-    filename = "./output/weatherapi_current.csv"
+    filename = f"./output/{str_today}_weatherapi_current.csv"
     weatherapi_current_df.to_csv(filename, index=False, header=True)
 
     # weatherapi - daily
     weatherapi_daily_df = weather_api.get_daily(township_df, no_of_days=7)
-    filename = "./output/weatherapi_daily.csv"
+    filename = f"./output/{str_today}_weatherapi_daily.csv"
     weatherapi_daily_df.to_csv(filename, index=False, header=True)
 
     openmeteo_api = OpenMeteoAPI()
 
     # open-meteo - current
     openmeteo_current_df = openmeteo_api.get_current(township_df)
-    filename = "./output/open_meteo_current.csv"
+    filename = f"./output/{str_today}_open_meteo_current.csv"
     openmeteo_current_df.to_csv(filename, index=False, header=True)
 
     # open-meteo - daily
     openmeteo_daily_df = openmeteo_api.get_daily(township_df)
-    filename = "./output/open_meteo_daily.csv"
+    filename = f"./output/{str_today}_open_meteo_daily.csv"
     openmeteo_daily_df.to_csv(filename, index=False, header=True)
 
     # Fetch AmbientWeather data
     ambient_api = AmbientWeatherAPI()  # New instance for AmbientWeatherAPI
+
     ambient_df = ambient_api.get_forecast_df(township_df)  # Get forecast data
-    ambient_df.to_csv("./output/ambientweather_data.csv", index=False, header=True)
+    filename = f"./output/{str_today}_ambientweather_data.csv"
+    ambient_df.to_csv(filename, index=False, header=True)
+
+
+if __name__ == "__main__":
+    fetch_weather_data()
