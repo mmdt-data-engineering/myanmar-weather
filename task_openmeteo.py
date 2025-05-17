c96e5d45a87415c620c3371970cb4d05e16cf9c1
Myo Thet <mr.myothet@gmail.com>
2025-05-17 21:49:05 +0700

    openmeteo task

---
+++ task_openmeteo.py
@@ -0,0 +1,45 @@
+from datetime import date
+from openmeteo_api import OpenMeteoAPI
+import pandas as pd
+from load_to_db import load_file_to_db
+from data_utils import MIMU_Data
+from Logger import Logger
+
+
+def print_info(message: str):
+    print(message)
+    logger = Logger().get_logger("OpenMeteo Task")
+    logger.info(message)
+
+
+def openmeteo_task():
+    print_info("starting the task...")
+
+    print_info("getting townships from MIMU data")
+    mimu = MIMU_Data()
+    township_df = mimu.get_townships()
+    township_df = township_df.head(5)
+
+    print_info("extracting data from api and save as csv file")
+    openmeteo_api = OpenMeteoAPI()
+
+    # openmeteo - current
+    openmeteo_current_df = openmeteo_api.get_current(township_df)
+
+    str_today = date.today().strftime("%Y-%m-%d")  # Output like '2025-05-16'
+    file_path = f"./output/{str_today}_openmeteo_current.csv"
+    openmeteo_current_df.to_csv(file_path, index=False, header=True)
+
+    print_info("load csv file to database")
+    load_file_to_db(file_path)
+
+    # openmeteo - daily
+    openmeteo_daily_df = openmeteo_api.get_daily(township_df)
+    file_path = f"./output/{str_today}_openmeteo_forecast.csv"
+    openmeteo_daily_df.to_csv(file_path, index=False, header=True)
+
+    print_info("load csv file to database")
+    load_file_to_db(file_path)
+
+
+openmeteo_task()
