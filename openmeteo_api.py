a7d88e6550077a3ee3eaa1290b034e1037a61f0a
Myo Thet <mr.myothet@gmail.com>
2025-05-19 22:47:03 +0700

    update openmeteo api - use requests instead of openmeteo.weather_api

--- openmeteo_api.py
+++ openmeteo_api.py
@@ -6,6 +6,8 @@ from retry_requests import retry
 import time
 from openmeteo_attributes import current_attributes, daily_attributes
 from Logger import Logger
+import requests
+import json
 
 
 class OpenMeteoAPI:
@@ -46,7 +48,6 @@ class OpenMeteoAPI:
             sleep_time = random.uniform(1, 5)
             message = f"Sleeping for {sleep_time:.2f} seconds..."
             self.print_info(message)
-
             time.sleep(sleep_time)
 
             df = self._get_current(latitude, longitude)
@@ -69,7 +70,6 @@ class OpenMeteoAPI:
             sleep_time = random.uniform(1, 5)
             message = f"Sleeping for {sleep_time:.2f} seconds..."
             self.print_info(message)
-
             time.sleep(sleep_time)
 
             df = self._get_daily(latitude, longitude)
@@ -79,82 +79,188 @@ class OpenMeteoAPI:
 
     def _get_current(self, latitude: float, longitude: float) -> pd.DataFrame:
 
-        # Setup the Open-Meteo API client with cache and retry on error
-        cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
-        retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-        openmeteo = openmeteo_requests.Client(session=retry_session)
-
-        # Make sure all required weather variables are listed here
-        # The order of variables in hourly or daily is important to assign them correctly below
         url = "https://api.open-meteo.com/v1/forecast"
         params = {
             "latitude": latitude,
             "longitude": longitude,
             "current": current_attributes,
         }
-        responses = openmeteo.weather_api(url, params=params)
 
-        # Process first location. Add a for-loop for multiple locations or weather models
-        response = responses[0]
+        # responses = openmeteo.weather_api(url, params=params)
+        res = requests.get(url, params=params)
 
-        # Current values. The order of variables needs to be the same as requested.
-        current = response.Current()
+        response = json.loads(res.text)
 
-        current_data = {
-            "date": pd.date_range(
-                start=pd.to_datetime(current.Time(), unit="s", utc=True),
-                end=pd.to_datetime(current.TimeEnd(), unit="s", utc=True),
-                freq=pd.Timedelta(seconds=current.Interval()),
-                inclusive="left",
-            )
-        }
+        message = f"Coordinates {response['latitude']}°N {response['longitude']}°E"
+        self.print_info(message)
+        message = f"Elevation {response['elevation']} m asl"
+        self.print_info(message)
+        message = f"Timezone {response['timezone']}{response['timezone_abbreviation']}"
+        self.print_info(message)
+        message = f"Timezone difference to GMT+0 {response['utc_offset_seconds']} s"
+        self.print_info(message)
 
-        for i in range(current.VariablesLength()):
-            variable = current.Variables(i)
-            attribute = variable.Variable()
-            if attribute in current_attributes:
-                print(f"{attribute}: {variable.ValuesAsNumpy()}")
-                current_data[attribute] = variable.ValuesAsNumpy()
+        current_units = response["current_units"]
+        current = response["current"]
 
-        df = pd.DataFrame(data=current_data)
+        current_list = []
+
+        current_list.append(
+            {
+                "date": current["time"],
+                "date_units": current_units["time"],
+                "latitude": response["latitude"],
+                "longitude": response["longitude"],
+                "generationtime_ms": response["generationtime_ms"],
+                "utc_offset_seconds": response["utc_offset_seconds"],
+                "timezone": response["timezone"],
+                "timezone_abbreviation": response["timezone_abbreviation"],
+                "elevation": response["elevation"],
+                "interval": current["interval"],
+                "interval_units": current_units["interval"],
+                "temperature_2m": current["temperature_2m"],
+                "temperature_2m_units": current_units["temperature_2m"],
+                "relative_humidity_2m": current["relative_humidity_2m"],
+                "relative_humidity_2m_units": current_units["relative_humidity_2m"],
+                "apparent_temperature": current["apparent_temperature"],
+                "apparent_temperature_units": current_units["apparent_temperature"],
+                "is_day": current["is_day"],
+                "is_day_units": current_units["is_day"],
+                "wind_speed_10m": current["wind_speed_10m"],
+                "wind_speed_10m_units": current_units["wind_speed_10m"],
+                "wind_direction_10m": current["wind_direction_10m"],
+                "wind_direction_10m_units": current_units["wind_direction_10m"],
+                "wind_gusts_10m": current["wind_gusts_10m"],
+                "wind_gusts_10m_units": current_units["wind_gusts_10m"],
+                "precipitation": current["precipitation"],
+                "precipitation_units": current_units["precipitation"],
+                "showers": current["showers"],
+                "showers_units": current_units["showers"],
+                "snowfall": current["snowfall"],
+                "snowfall_units": current_units["snowfall"],
+                "rain": current["rain"],
+                "rain_units": current_units["rain"],
+                "weather_code": current["weather_code"],
+                "weather_code_units": current_units["weather_code"],
+                "cloud_cover": current["cloud_cover"],
+                "cloud_cover_units": current_units["cloud_cover"],
+                "pressure_msl": current["pressure_msl"],
+                "pressure_msl_units": current_units["pressure_msl"],
+                "surface_pressure": current["surface_pressure"],
+                "surface_pressure_units": current_units["surface_pressure"],
+            }
+        )
+
+        df = pd.DataFrame(current_list)
 
         return df
 
     def _get_daily(self, latitude: float, longitude: float) -> pd.DataFrame:
-        # Setup the Open-Meteo API client with cache and retry on error
-        cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
-        retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-        openmeteo = openmeteo_requests.Client(session=retry_session)
 
-        # Make sure all required weather variables are listed here
-        # The order of variables in hourly or daily is important to assign them correctly below
         url = "https://api.open-meteo.com/v1/forecast"
         params = {
             "latitude": latitude,
             "longitude": longitude,
             "daily": daily_attributes,
         }
-        responses = openmeteo.weather_api(url, params=params)
+        # responses = openmeteo.weather_api(url, params=params)
+        res = requests.get(url, params=params)
 
-        # Process first location. Add a for-loop for multiple locations or weather models
-        response = responses[0]
+        response = json.loads(res.text)
 
-        # Process daily data. The order of variables needs to be the same as requested.
-        daily = response.Daily()
+        message = f"Coordinates {response['latitude']}°N {response['longitude']}°E"
+        self.print_info(message)
+        message = f"Elevation {response['elevation']} m asl"
+        self.print_info(message)
+        message = f"Timezone {response['timezone']}{response['timezone_abbreviation']}"
+        self.print_info(message)
+        message = f"Timezone difference to GMT+0 {response['utc_offset_seconds']} s"
+        self.print_info(message)
 
-        daily_data = {
-            "date": pd.date_range(
-                start=pd.to_datetime(daily.Time(), unit="s", utc=True),
-                end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
-                freq=pd.Timedelta(seconds=daily.Interval()),
-                inclusive="left",
+        daily = response["daily"]
+        daily_units = response["daily_units"]
+
+        daily_list = []
+
+        for i in range(len(response["daily"]["time"])):
+            daily_list.append(
+                {
+                    "date": response["daily"]["time"][i],
+                    "date_units": daily_units["time"],
+                    "latitude": response["latitude"],
+                    "longitude": response["longitude"],
+                    "generationtime_ms": response["generationtime_ms"],
+                    "utc_offset_seconds": response["utc_offset_seconds"],
+                    "timezone": response["timezone"],
+                    "timezone_abbreviation": response["timezone_abbreviation"],
+                    "elevation": response["elevation"],
+                    "weather_code": daily["weather_code"][i],
+                    "weather_code_units": daily_units["weather_code"],
+                    "temperature_2m_max": daily["temperature_2m_max"][i],
+                    "temperature_2m_max_units": daily_units["temperature_2m_max"],
+                    "temperature_2m_min": daily["temperature_2m_min"][i],
+                    "temperature_2m_min_units": daily_units["temperature_2m_min"],
+                    "apparent_temperature_max": daily["apparent_temperature_max"][i],
+                    "apparent_temperature_max_units": daily_units[
+                        "apparent_temperature_max"
+                    ],
+                    "apparent_temperature_min": daily["apparent_temperature_min"][i],
+                    "apparent_temperature_min_units": daily_units[
+                        "apparent_temperature_min"
+                    ],
+                    "sunrise": daily["sunrise"][i],
+                    "sunrise_units": daily_units["sunrise"],
+                    "sunset": daily["sunset"][i],
+                    "sunset_units": daily_units["sunset"],
+                    "daylight_duration": daily["daylight_duration"][i],
+                    "daylight_duration_units": daily_units["daylight_duration"],
+                    "sunshine_duration": daily["sunshine_duration"][i],
+                    "sunshine_duration_units": daily_units["sunshine_duration"],
+                    "uv_index_max": daily["uv_index_max"][i],
+                    "uv_index_max_units": daily_units["uv_index_max"],
+                    "uv_index_clear_sky_max": daily["uv_index_clear_sky_max"][i],
+                    "uv_index_clear_sky_max_units": daily_units[
+                        "uv_index_clear_sky_max"
+                    ],
+                    "rain_sum": daily["rain_sum"][i],
+                    "rain_sum_units": daily_units["rain_sum"],
+                    "showers_sum": daily["showers_sum"][i],
+                    "showers_sum_units": daily_units["showers_sum"],
+                    "snowfall_sum": daily["snowfall_sum"][i],
+                    "snowfall_sum_units": daily_units["snowfall_sum"],
+                    "precipitation_sum": daily["precipitation_sum"][i],
+                    "precipitation_sum_units": daily_units["precipitation_sum"],
+                    "precipitation_hours": daily["precipitation_hours"][i],
+                    "precipitation_hours_units": daily_units["precipitation_hours"],
+                    "precipitation_probability_max": daily[
+                        "precipitation_probability_max"
+                    ][i],
+                    "precipitation_probability_max_units": daily_units[
+                        "precipitation_probability_max"
+                    ],
+                    "wind_speed_10m_max": daily["wind_speed_10m_max"][i],
+                    "wind_speed_10m_max_units": daily_units["wind_speed_10m_max"],
+                    "wind_gusts_10m_max": daily["wind_gusts_10m_max"][i],
+                    "wind_gusts_10m_max_units": daily_units["wind_gusts_10m_max"],
+                    "wind_direction_10m_dominant": daily["wind_direction_10m_dominant"][
+                        i
+                    ],
+                    "wind_direction_10m_dominant_units": daily_units[
+                        "wind_direction_10m_dominant"
+                    ],
+                    "shortwave_radiation_sum": daily["shortwave_radiation_sum"][i],
+                    "shortwave_radiation_sum_units": daily_units[
+                        "shortwave_radiation_sum"
+                    ],
+                    "et0_fao_evapotranspiration": daily["et0_fao_evapotranspiration"][
+                        i
+                    ],
+                    "et0_fao_evapotranspiration_units": daily_units[
+                        "et0_fao_evapotranspiration"
+                    ],
+                }
             )
-        }
 
-        for i, attribute in enumerate(daily_attributes):
-            # print(f"{attribute}: {daily.Variables(i).ValuesAsNumpy()}")
-            daily_data[str(attribute)] = daily.Variables(i).ValuesAsNumpy()
-
-        df = pd.DataFrame(data=daily_data)
+        df = pd.DataFrame(daily_list)
 
         return df
