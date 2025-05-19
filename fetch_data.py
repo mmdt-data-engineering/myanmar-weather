25ef34ec29f4f13fcaf871e66e1580b64dee261d
Myo Thet <mr.myothet@gmail.com>
2025-05-19 19:41:00 +0700

    implement retry - 3 times - retry delay - 3 seconds

--- fetch_data.py
+++ fetch_data.py
@@ -1,7 +1,10 @@
 import aiohttp
 import asyncio
 from Logger import Logger
+import time
 
+max_retries = 3
+retry_delay = 3  # Seconds
 
 def print_error(message: str):
     print(message)
@@ -9,17 +12,26 @@ def print_error(message: str):
     logger.error(message)
 
 async def fetch(url, headers=None):
-    try:
-        async with aiohttp.ClientSession() as session:
-            if headers == None:
-                async with session.get(url) as response:
-                    return await response.text()
-            else: 
-                async with session.get(url, headers=headers) as response:
-                    return await response.text()
-    except aiohttp.ClientError as e:  # Catch general aiohttp errors
-        print_error(f"Request error: {e}")
-    except asyncio.TimeoutError:  # Catch timeout errors
-        print_error("Request timed out")
-    except Exception as e:  # Catch unexpected errors
-        print_error(f"Unexpected error: {e}")
+    for attempt in range(max_retries):
+        try:
+            async with aiohttp.ClientSession() as session:
+                if headers == None:
+                    async with session.get(url) as response:
+                        return await response.text()
+                else: 
+                    async with session.get(url, headers=headers) as response:
+                        return await response.text()
+        except aiohttp.ClientError as e:  # Catch general aiohttp errors
+            print_error(f"Request error: {e}")
+            print_error(f"Attempt {attempt+1} failed: {e}")
+            time.sleep(retry_delay)
+        except asyncio.TimeoutError:  # Catch timeout errors
+            print_error("Request timed out")
+            print_error(f"Attempt {attempt+1} failed: {e}")
+            time.sleep(retry_delay)
+        except Exception as e:  # Catch unexpected errors
+            print_error(f"Unexpected error: {e}")
+            print_error(f"Attempt {attempt+1} failed: {e}")
+            time.sleep(retry_delay)
+    else:
+        print_error("All retries failed.")
