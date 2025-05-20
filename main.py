bb44dcb3908efaa890026e0a50f0e60e22beb5bb
Myo Thet <mr.myothet@gmail.com>
2025-05-20 18:17:41 +0700

    separate functions for each api call in main file

--- main.py
+++ main.py
@@ -5,55 +5,13 @@ from ambientweather_api import AmbientWeatherAPI
 from datetime import date
 from meteoblue_api import MeteoBlueWeatherAPI
 import asyncio
+import pandas as pd
+from time import time
+from time_utils import readable_time
 
 
-async def fetch_weather_data():
-    # Get townships from MIMU data
-    mimu_data = MIMU_Data()
-    township_df = mimu_data.get_townships()
-
-    township_df = township_df.head(50)
-
-    today = date.today()
-    str_today = today.strftime("%Y-%m-%d")  # Output like '2025-05-16'
-
-    # weatherapi - current
-    weather_api = WeatherAPI()
-    weatherapi_current_df = await weather_api.get_current(township_df)
-
-    if weatherapi_current_df is None or weatherapi_current_df.empty:
-        raise ValueError("WeatherAPI current daraframe is empty")
-    else:
-        filename = f"./output/{str_today}_weatherapi_current.csv"
-        weatherapi_current_df.to_csv(filename, index=False, header=True)
-
-    # weatherapi - daily
-    weatherapi_daily_df = await weather_api.get_daily(township_df, no_of_days=7)
-    if weatherapi_daily_df is None or weatherapi_daily_df.empty: 
-        raise ValueError("WeatherAPI daily dataframe is empty")
-    else:
-        filename = f"./output/{str_today}_weatherapi_forecast.csv"
-        weatherapi_daily_df.to_csv(filename, index=False, header=True)
-
-    openmeteo_api = OpenMeteoAPI()
-
-    # open-meteo - current
-    openmeteo_current_df = await openmeteo_api.get_current(township_df)
-
-    if openmeteo_current_df is None or openmeteo_current_df.empty:
-        raise ValueError("open-meteo current dataframe is empty")
-    else:
-        filename = f"./output/{str_today}_open_meteo_current.csv"
-        openmeteo_current_df.to_csv(filename, index=False, header=True)
-
-    # open-meteo - daily
-    openmeteo_daily_df = await openmeteo_api.get_daily(township_df)
-
-    if openmeteo_daily_df is None or openmeteo_daily_df.empty:
-        raise ValueError("open-meteo daily dataframe is empty")
-    else:
-        filename = f"./output/{str_today}_open_meteo_forecast.csv"
-        openmeteo_daily_df.to_csv(filename, index=False, header=True)
+async def fetch_ambient_data(township_df: pd.DataFrame):
+    today = date.today().strftime("%Y-%m-%d")  # Output like '2025-05-16'
 
     # Fetch AmbientWeather data
     ambient_api = AmbientWeatherAPI()  # New instance for AmbientWeatherAPI
@@ -63,8 +21,13 @@ async def fetch_weather_data():
     if ambient_df is None or ambient_df.empty:
         raise ValueError("Ambient dataframe is empty")
     else:
-        filename = f"./output/{str_today}_ambientweather_data_forecast.csv"
+        filename = f"./output/{today}_ambientweather_data_forecast.csv"
         ambient_df.to_csv(filename, index=False, header=True)
+
+
+async def fetch_meteoblue_data(township_df: pd.DataFrame):
+
+    today = date.today().strftime("%Y-%m-%d")  # Output like '2025-05-16'
 
     # Fetch MeteoBlue data
     meteoblue_api = MeteoBlueWeatherAPI()
@@ -73,7 +36,7 @@ async def fetch_weather_data():
     if meteoblue_df is None or meteoblue_df.empty: 
         raise ValueError("meteo-blue current dataframe is empty")
     else: 
-        filename = f"./output/{str_today}_meteoblue_current.csv"
+        filename = f"./output/{today}_meteoblue_current.csv"
         meteoblue_df.to_csv(filename, index=False, header=True)
 
     meteoblue_df = await meteoblue_api.get_meteoblue_forecast_weather_data(township_df)
@@ -81,9 +44,76 @@ async def fetch_weather_data():
     if meteoblue_df is None or meteoblue_df.empty:
         raise ValueError("meteo-blue daily dataframe is empty")
     else: 
-        filename = f"./output/{str_today}_meteoblue_forecast.csv"
+        filename = f"./output/{today}_meteoblue_forecast.csv"
         meteoblue_df.to_csv(filename, index=False, header=True)
 
 
+async def fetch_openmeteo_data(township_df: pd.DataFrame):
+
+    today = date.today().strftime("%Y-%m-%d")  # Output like '2025-05-16'
+
+    openmeteo_api = OpenMeteoAPI()
+
+    # open-meteo - current
+    openmeteo_current_df = await openmeteo_api.get_current(township_df)
+
+    if openmeteo_current_df is None or openmeteo_current_df.empty:
+        raise ValueError("open-meteo current dataframe is empty")
+    else:
+        filename = f"./output/{today}_open_meteo_current.csv"
+        openmeteo_current_df.to_csv(filename, index=False, header=True)
+
+    # open-meteo - daily
+    openmeteo_daily_df = await openmeteo_api.get_daily(township_df)
+
+    if openmeteo_daily_df is None or openmeteo_daily_df.empty:
+        raise ValueError("open-meteo daily dataframe is empty")
+    else:
+        filename = f"./output/{today}_open_meteo_forecast.csv"
+        openmeteo_daily_df.to_csv(filename, index=False, header=True)
+
+
+async def fetch_weatherapi_data(township_df:pd.DataFrame):
+
+    today = date.today().strftime("%Y-%m-%d")  # Output like '2025-05-16'
+
+    # weatherapi - current
+    weather_api = WeatherAPI()
+    weatherapi_current_df = await weather_api.get_current(township_df)
+
+    if weatherapi_current_df is None or weatherapi_current_df.empty:
+        raise ValueError("WeatherAPI current daraframe is empty")
+    else:
+        filename = f"./output/{today}_weatherapi_current.csv"
+        weatherapi_current_df.to_csv(filename, index=False, header=True)
+
+    # weatherapi - daily
+    weatherapi_daily_df = await weather_api.get_daily(township_df, no_of_days=7)
+    if weatherapi_daily_df is None or weatherapi_daily_df.empty: 
+        raise ValueError("WeatherAPI daily dataframe is empty")
+    else:
+        filename = f"./output/{today}_weatherapi_forecast.csv"
+        weatherapi_daily_df.to_csv(filename, index=False, header=True)
+
 if __name__ == "__main__":
-    asyncio.run(fetch_weather_data())
+
+    start_time = time()
+
+    # Get townships from MIMU data
+    mimu_data = MIMU_Data()
+    township_df = mimu_data.get_townships()
+    township_df = township_df.head(50)
+
+    asyncio.run(fetch_ambient_data(township_df))
+    
+    asyncio.run(fetch_meteoblue_data(township_df))
+    
+    asyncio.run(fetch_openmeteo_data(township_df))
+    
+    asyncio.run(fetch_weatherapi_data(township_df))
+
+    end_time = time()
+    time_taken_seconds = end_time - start_time
+    hours, minutes, seconds = readable_time(time_taken_seconds)
+    print(f"Total time taken was {hours} hours, {minutes} minutes, {seconds} seconds.")
+
