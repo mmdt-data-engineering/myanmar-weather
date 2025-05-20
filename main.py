0d2ee33ac91c5f788c86a5efc7e7e24c78543109
Myo Thet <mr.myothet@gmail.com>
2025-05-20 16:13:07 +0700

    fix - coroutine OpenMeteoAPI.get_current was never awaited

--- main.py
+++ main.py
@@ -32,12 +32,12 @@ async def fetch_weather_data():
     openmeteo_api = OpenMeteoAPI()
 
     # open-meteo - current
-    openmeteo_current_df = openmeteo_api.get_current(township_df)
+    openmeteo_current_df = await openmeteo_api.get_current(township_df)
     filename = f"./output/{str_today}_open_meteo_current.csv"
     openmeteo_current_df.to_csv(filename, index=False, header=True)
 
     # open-meteo - daily
-    openmeteo_daily_df = openmeteo_api.get_daily(township_df)
+    openmeteo_daily_df = await openmeteo_api.get_daily(township_df)
     filename = f"./output/{str_today}_open_meteo_forecast.csv"
     openmeteo_daily_df.to_csv(filename, index=False, header=True)
 
