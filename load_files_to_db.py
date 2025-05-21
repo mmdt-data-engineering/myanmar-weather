e9bef6812a471ed4014b4007ce0bc3481967e6c1
Myo Thet <mr.myothet@gmail.com>
2025-05-21 22:21:29 +0700

    just use output folder

--- load_files_to_db.py
+++ load_files_to_db.py
@@ -6,10 +6,7 @@ from datetime import date
 def load_all_csv_files_to_db():
     """Load CSV files from a folder to Neon PostgreSQL database."""
 
-    today = date.today()
-    print(f"Today's date: {today}")
-
-    folder_path = "./output/{today}/"
+    folder_path = "./output/"
     csv_files = get_csv_files(folder_path)
 
     for file in csv_files:
