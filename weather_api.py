6ccb3dcb258f57b092bc7bcb09496e9a920c5ff5
Myo Thet <mr.myothet@gmail.com>
2025-05-22 18:59:09 +0700

    weatherapi - add date column to be the same columns with other api

--- weather_api.py
+++ weather_api.py
@@ -63,7 +63,7 @@ class WeatherAPI:
         # response = requests.get(url)
         response, status = await fetch(url)
 
-        if status != 200: 
+        if status != 200:
             raise ConnectionError(f"Fetch data from Weather API - FAILED ")
 
         data = json.loads(response)
@@ -75,6 +75,7 @@ class WeatherAPI:
         current_list = []
         current_list.append(
             {
+                "date": current.get("last_updated", None),
                 "town": location.get("name", None),
                 "region": location.get("region", None),
                 "country": location.get("country", None),
@@ -174,7 +175,7 @@ class WeatherAPI:
         # response = requests.get(url)
         response, status = await fetch(url)
 
-        if status != 200: 
+        if status != 200:
             raise ConnectionError(f"Fetch data from Weather API - FAILED ")
 
         data = json.loads(response)
@@ -201,7 +202,6 @@ class WeatherAPI:
         # Combine location and forecast
         final_df = pd.concat([forecast_df, location_repeated], axis=1)
 
-
         # remove the prefix from column names
         final_df.columns = final_df.columns.str.replace("day.", "")
         final_df.columns = final_df.columns.str.replace("condition.", "")
@@ -215,39 +215,28 @@ class WeatherAPI:
         ]
         final_df = final_df.drop(columns=columns_to_drop, errors="ignore")
 
-
         # Rename to standardized column names
         standardized_columns = {
             "date": "date",
             "date_epoch": "date_epoch",
-
             "maxtemp_c": "temperature_max_celsius",
             "mintemp_c": "temperature_min_celsius",
             "avgtemp_c": "temperature_mean_celsius",
-
             "maxwind_mph": "wind_speed_max_mph",
             "maxwind_kph": "wind_speed_max_kph",
-    
             "totalprecip_mm": "precipitation_millimeters",
             "totalsnow_cm": "snow_total_centimeters",
-
             "avgvis_km": "visibility_avg_kilometers",
             "avgvis_miles": "visibility_average_miles",
-
             "avghumidity": "humidity_average",
-
             "daily_will_it_rain": "will_it_rain",
             "daily_chance_of_rain": "chance_of_rain",
-
             "daily_will_it_snow": "will_it_snow",
             "daily_chance_of_snow": "chance_of_snow",
-
             "text": "weather_text",
             "icon": "weather_icon_url",
             "code": "weather_code",
-
             "uv": "uv_index",
-
             "name": "township_name",
             "region": "region",
             "country": "country",
@@ -256,11 +245,10 @@ class WeatherAPI:
             "tz_id": "timezone",
             "localtime_epoch": "localtime_epoch",
             "localtime": "localtime",
-
             "town_name": "town_name",
             "district_name": "district_name",
             "state_name": "state_name",
-            }
+        }
 
         # Apply the rename
         final_df = final_df.rename(columns=standardized_columns)
@@ -268,7 +256,7 @@ class WeatherAPI:
         final_df["town_name"] = town_name
         final_df["district_name"] = district_name
         final_df["state_name"] = state_name
-        final_df.insert(0, "source", "weatherapi")        
+        final_df.insert(0, "source", "weatherapi")
         final_df.insert(1, "extraction_date", pd.to_datetime(time.strftime("%Y-%m-%d")))
 
         print(final_df.columns)
