a764765042bd1d9560be1efafb4b739d9ae68e3d
Myo Thet <mr.myothet@gmail.com>
2025-05-22 09:44:07 +0700

    openmeteo - rename column names referenced from meteoblue

--- openmeteo_api.py
+++ openmeteo_api.py
@@ -10,6 +10,7 @@ import requests
 import json
 from fetch_data import fetch
 from data_utils import get_weather_description
+from datetime import date
 
 
 class OpenMeteoAPI:
@@ -113,10 +114,13 @@ class OpenMeteoAPI:
 
         current_list = []
 
+        today = date.today().strftime("%Y-%m-%d")
+
         data = {
             "date": pd.to_datetime(current["time"]),
-            "extration_date": pd.to_datetime(time.strftime("%Y-%m-%d")),
-            "region": region,
+            # Add today's date as extraction date
+            "extration_date": pd.to_datetime(today),
+            "state": region,
             "district": district,
             "township": township,
             "latitude": response["latitude"],
@@ -127,40 +131,20 @@ class OpenMeteoAPI:
             "weather_description": get_weather_description(
                 int(current["weather_code"])
             ),
-            "temperature_2m_celsius": current["temperature_2m"],
-            "relative_humidity_2m_percent": current["relative_humidity_2m"],
+            "temperature_celsius": current["temperature_2m"],
+            "relative_humidity_percent": current["relative_humidity_2m"],
             "apparent_temperature_celsius": current["apparent_temperature"],
             "is_day": current["is_day"],
-            "wind_speed_10m_kmh": current["wind_speed_10m"],
-            "wind_direction_10m_degree": current["wind_direction_10m"],
-            "wind_gusts_10m_kmh": current["wind_gusts_10m"],
-            "precipitation_mm": current["precipitation"],
-            "showers_mm": current["showers"],
-            "snowfall_cm": current["snowfall"],
-            "rain_mm": current["rain"],
+            "wind_speed_kilometers_per_hour": current["wind_speed_10m"],
+            "wind_direction_degree": current["wind_direction_10m"],
+            "wind_gusts_kilometers_per_hour": current["wind_gusts_10m"],
+            "precipitation_millimeters": current["precipitation"],
+            "showers_millimeters": current["showers"],
+            "snowfall_centimeters": current["snowfall"],
+            "rain_millimeters": current["rain"],
             "cloud_cover_percent": current["cloud_cover"],
-            "pressure_msl_hectopascal": current["pressure_msl"],
-            "surface_pressure_hectopascal": current["surface_pressure"],
-            # "generationtime_ms": response["generationtime_ms"],
-            # "utc_offset_seconds": response["utc_offset_seconds"],
-            # "timezone": response["timezone"],
-            # "timezone_abbreviation": response["timezone_abbreviation"],
-            # "interval_units": current_units["interval"],
-            # "temperature_2m_units": current_units["temperature_2m"],
-            # "relative_humidity_2m_units": current_units["relative_humidity_2m"],
-            # "apparent_temperature_units": current_units["apparent_temperature"],
-            # "is_day_units": current_units["is_day"],
-            # "wind_speed_10m_units": current_units["wind_speed_10m"],
-            # "wind_direction_10m_units": current_units["wind_direction_10m"],
-            # "wind_gusts_10m_units": current_units["wind_gusts_10m"],
-            # "precipitation_units": current_units["precipitation"],
-            # "showers_units": current_units["showers"],
-            # "snowfall_units": current_units["snowfall"],
-            # "rain_units": current_units["rain"],
-            # "weather_code_units": current_units["weather_code"],
-            # "cloud_cover_units": current_units["cloud_cover"],
-            # "pressure_msl_units": current_units["pressure_msl"],
-            # "surface_pressure_units": current_units["surface_pressure"],
+            "pressure_msl_hectopascals": current["pressure_msl"],
+            "surface_pressure_hectopascals": current["surface_pressure"],
         }
 
         current_list.append(data)
@@ -216,8 +200,8 @@ class OpenMeteoAPI:
                 "weather_description": get_weather_description(
                     int(daily["weather_code"][i])
                 ),
-                "temperature_2m_max_celsius": daily["temperature_2m_max"][i],
-                "temperature_2m_min_celsius": daily["temperature_2m_min"][i],
+                "temperature_max_celsius": daily["temperature_2m_max"][i],
+                "temperature_min_celsius": daily["temperature_2m_min"][i],
                 "apparent_temperature_max_celsius": daily["apparent_temperature_max"][
                     i
                 ],
@@ -230,58 +214,24 @@ class OpenMeteoAPI:
                 "sunshine_duration_second": daily["sunshine_duration"][i],
                 "uv_index_max": daily["uv_index_max"][i],
                 "uv_index_clear_sky_max": daily["uv_index_clear_sky_max"][i],
-                "rain_sum_mm": daily["rain_sum"][i],
-                "showers_sum_mm": daily["showers_sum"][i],
-                "snowfall_sum_cm": daily["snowfall_sum"][i],
-                "precipitation_sum_mm": daily["precipitation_sum"][i],
+                "rain_sum_millimeters": daily["rain_sum"][i],
+                "showers_sum_millimeters": daily["showers_sum"][i],
+                "snowfall_sum_centimeters": daily["snowfall_sum"][i],
+                "precipitation_sum_millimeters": daily["precipitation_sum"][i],
                 "precipitation_hours": daily["precipitation_hours"][i],
                 "precipitation_probability_max_percent": daily[
                     "precipitation_probability_max"
                 ][i],
-                "wind_speed_10m_max_kmh": daily["wind_speed_10m_max"][i],
-                "wind_gusts_10m_max_kmh": daily["wind_gusts_10m_max"][i],
-                "wind_direction_10m_dominant_degree": daily[
-                    "wind_direction_10m_dominant"
-                ][i],
+                "wind_speed_max_kilometers_per_hour": daily["wind_speed_10m_max"][i],
+                "wind_gusts_max_kilometers_per_hour": daily["wind_gusts_10m_max"][i],
+                "wind_direction_dominant_degree": daily["wind_direction_10m_dominant"][
+                    i
+                ],
+                # mjm2 = mega joules per square meter
                 "shortwave_radiation_sum_mjm2": daily["shortwave_radiation_sum"][i],
-                "et0_fao_evapotranspiration_mm": daily["et0_fao_evapotranspiration"][i],
-                # "date_units": daily_units["time"],
-                # "generationtime_ms": response["generationtime_ms"],
-                # "utc_offset_seconds": response["utc_offset_seconds"],
-                # "timezone": response["timezone"],
-                # "timezone_abbreviation": response["timezone_abbreviation"],
-                # "weather_code_units": daily_units["weather_code"],
-                # "temperature_2m_max_units": daily_units["temperature_2m_max"],
-                # "temperature_2m_min_units": daily_units["temperature_2m_min"],
-                # "apparent_temperature_max_units": daily_units[
-                # "apparent_temperature_max"
-                # ],
-                # "apparent_temperature_min_units": daily_units[
-                #     "apparent_temperature_min"
-                # ],
-                # "sunrise_units": daily_units["sunrise"],
-                # "sunset_units": daily_units["sunset"],
-                # "daylight_duration_units": daily_units["daylight_duration"],
-                # "sunshine_duration_units": daily_units["sunshine_duration"],
-                # "uv_index_max_units": daily_units["uv_index_max"],
-                # "uv_index_clear_sky_max_units": daily_units["uv_index_clear_sky_max"],
-                # "rain_sum_units": daily_units["rain_sum"],
-                # "showers_sum_units": daily_units["showers_sum"],
-                # "snowfall_sum_units": daily_units["snowfall_sum"],
-                # "precipitation_sum_units": daily_units["precipitation_sum"],
-                # "precipitation_hours_units": daily_units["precipitation_hours"],
-                # "precipitation_probability_max_units": daily_units[
-                #     "precipitation_probability_max"
-                # ],
-                # "wind_speed_10m_max_units": daily_units["wind_speed_10m_max"],
-                # "wind_gusts_10m_max_units": daily_units["wind_gusts_10m_max"],
-                # "wind_direction_10m_dominant_units": daily_units[
-                #     "wind_direction_10m_dominant"
-                # ],
-                # "shortwave_radiation_sum_units": daily_units["shortwave_radiation_sum"],
-                # "et0_fao_evapotranspiration_units": daily_units[
-                #     "et0_fao_evapotranspiration"
-                # ],
+                "et0_fao_evapotranspiration_milimeters": daily[
+                    "et0_fao_evapotranspiration"
+                ][i],
             }
 
             daily_list.append(data)
